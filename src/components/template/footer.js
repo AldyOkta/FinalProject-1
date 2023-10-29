c33fe08afbdd4745f5bfe340f99baa6a38bdb7f4
raflymuhammad092 <raflymuhammad092@gmail.com>
2023-10-30 00:17:17 +0700

    Final1

--- src/components/template/footer.js
+++ src/components/template/footer.js
@@ -1,9 +1,10 @@
 import React from "react";
-
+import "./footer.css";
 const Footer = () => {
   return (
     <div>
       <div className="footer">
+        <div className="logo"></div>
         <div className="a">
           <p>Copyright Kelompok 8</p>
         </div>
